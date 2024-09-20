b342bbceb95fa2194a493b25b4607e0f4ac6acf7
Sagarhejaji <155833336+Sagarhejaji@users.noreply.github.com>
2024-09-20 12:07:40 +0530

    Update passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,19 +1,58 @@
-const { Strategy, ExtractJwt } = require('passport-jwt');
-const passport = require('passport');
+require("dotenv").config();
+const User = require("../models/user.model");
+const Token = require("../models/token.model");
+const JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
+const passport = require("passport");
+const opts = {};
+const jwt = require("jsonwebtoken");
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.SECRET;
 
-const opts = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: process.env.JWT_SECRET, // Ensure this line is present
-};
+passport.use(
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    try {
+      const user = await User.findOne({ email: jwt_payload.email });
 
-const strategy = new Strategy(opts, (jwt_payload, done) => {
-    // Your logic to handle the JWT payload and authentication
-    // Example:
-    // User.findById(jwt_payload.id)
-    //     .then(user => {
-    //         return done(null, user);
-    //     })
-    //     .catch(err => done(err, false));
-});
+      if (user) {
+        const refreshTokenFromDB = await Token.findOne({
+          user: user._id,
+        });
 
-passport.use(strategy);
+        if (!refreshTokenFromDB) {
+          return done(null, false);
+        }
+
+        const refreshPayload = jwt.verify(
+          refreshTokenFromDB.refreshToken,
+          process.env.REFRESH_SECRET
+        );
+
+        if (refreshPayload.email !== jwt_payload.email) {
+          return done(null, false);
+        }
+
+        const tokenExpiration = new Date(jwt_payload.exp * 1000);
+        const now = new Date();
+        const timeDifference = tokenExpiration.getTime() - now.getTime();
+
+        if (timeDifference > 0 && timeDifference < 30 * 60 * 1000) {
+          const payloadNew = {
+            _id: user._id,
+            email: user.email,
+          };
+          const newToken = jwt.sign(payloadNew, process.env.SECRET, {
+            expiresIn: "6h",
+          });
+
+          return done(null, { user, newToken });
+        }
+        return done(null, { user });
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
